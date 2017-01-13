93615e1ec1948b08f948a4f732899bce6722fc00
nfsmaniac <nfsmaniac@pd-mail.tk>
2017-01-13 20:59:47 +0100

    Ability to set custom output filename/path

--- mkpatch.py
+++ mkpatch.py
@@ -7,13 +7,14 @@ import paktools
 
 def main():
   if len(sys.argv) <= 1:
-    print("Usage: {0} [stockPO] [editedPO]".format(sys.argv[0]))
+    print("Usage: {0} [stockPO] [editedPO] [output-patchPO]".format(sys.argv[0]))
     return
   
-  originalPo     = sys.argv[1]
-  editedPo    = sys.argv[2]
+  originalPo    = sys.argv[1]
+  editedPo      = sys.argv[2]
+  patchPo       = sys.argv[3]
   
-  paktools.CreatePatch(originalPo, editedPo)
+  paktools.CreatePatch(originalPo, editedPo, patchPo)
 
 if __name__ == '__main__':
   main()
